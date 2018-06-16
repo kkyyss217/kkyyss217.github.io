45380a3e8891e3140e4ee8eb7df9f8982a7e4cf5
kkyyss217 <kkyyss217@naver.com>
2018-06-17 00:02:26 +0900

    HW

--- HomeWork/wp_final_project/myjs.js
+++ HomeWork/wp_final_project/myjs.js
@@ -1,19 +1,19 @@
-function home(){
+function Home(){
 	location.href="page.html";
 }
-function project(){
+function Project(){
 	var view = document.getElementById("view");
 	view.innerHTML = '<a href="../paint/paint.html">Paint App</a></br></br>';
 	view.innerHTML += '<a href="../Bouncing Balls/bounce.html">Bouncing Balls</a></br></br>';
 	view.innerHTML += '<a href="../Bouncing Ball Game/game.html">Bouncing Ball Game</a></br></br>';
-	view.innerHTML += '<a href="../paint/paint.html">Web Editor</a></br></br>';
+	view.innerHTML += '<a href="../phonebookv2/phonebookv2.html">Phone Book</a></br></br>';
 	view.innerHTML += '<a href="../hw-firebase/Index.html">Web App using Firebase</a></br></br>';
 }
-function aboutme(){
+function Aboutme(){
 	var view = document.getElementById("view");
 	view.innerText="Suwon University  ( 15054051 )\n\n"+"Major : Information of Security\n\n"+"Myname is Choi Hyunkyu\n\n";
 }
-function links(){
+function Links(){
 	var view = document.getElementById("view");
-	view.innerHTML = '<a href="http://github.com"> GitHub</a></br></br>';	
+	view.innerHTML = '<a href="http://github.com">GitHub</a></br></br>';	
 }
